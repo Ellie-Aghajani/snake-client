c56763fe528d58f88e0803d78b5f67a903dba996
Ellie-Aghajani <z.aqajani@gmail.com>
2022-11-23 14:39:25 -0800

    connect exported as object

---
+++ client.js
@@ -0,0 +1,18 @@
+const net = require("net");
+// establishes a connection with the game server
+const connect = function () {
+    const conn = net.createConnection({
+      host: "165.227.47.243",
+      port:50541
+    });
+    conn.on("connect", () => {
+      console.log("you ded cuz you idled");
+    });
+  
+    // interpret incoming data as text
+    conn.setEncoding("utf8");
+  
+    return conn;
+  };
+
+  module.exports = {connect};
--- play.js
+++ play.js
@@ -1,20 +1,5 @@
 const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port:50541
-  });
-  conn.on("connect", () => {
-    console.log("you ded cuz you idled");
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+const {connect} = require('./client');
 
 console.log("Connecting ...");
 connect();
